1d74f92347c2416046c81e4ebd9d71748565890c
Tony Fernandez <tfernandez.sc2@gmail.com>
2020-11-30 23:21:14 -0500

    fixed theknock interval. It was doing seconds instead of minutes.

--- src/server.py
+++ src/server.py
@@ -4,33 +4,33 @@ import socket
 import time
 import os
 import select
+from datetime import datetime
 
 def knock(host, knock_seq, delay):
     """Knock host and port using tcp connection"""
     for port_proto in knock_seq:
         port = port_proto.split(':')[0]
         protocol = port_proto.split(':')[1]
-        print(port + ' ' + protocol)
         if protocol == 'tcp':
-            print("using TCP")
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.setblocking(False)
             sock.connect_ex((host,int(port)))
             select.select([sock], [sock], [sock], 0)
         else:
-            print("using UDP")
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             sock.setblocking(False)
             sock.sendto(b'', (host,int(port)))
 
         sock.close()
         time.sleep(int(delay))
-    time.sleep(int(interval))
+    time.sleep(int(interval)*60)
+    
 
 ip = os.getenv('IP', default='grandvista.duckdns.org')
 ports = os.getenv('PORTS', default='1992:tcp,2991:udp').split(',')
-interval = os.getenv('INTERVAL_IN_MIN', default='60')
+interval = os.getenv('INTERVAL_IN_MIN', default='2')
 delay = os.getenv('DELAY_IN_SEC', default='5')
 
 while True:
+    print(datetime.now())
     knock(ip, ports, delay)
